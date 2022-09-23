52df375df1052aabb73e1815fc0352912e6ab5dd
Farhan-ibni-zahoor <87404631+Farhan-ibni-zahoor@users.noreply.github.com>
2022-09-23 20:48:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+status = "";
+input_text = "";
+
+function setup() {
+    canvas = createCanvas(300,250);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(300,250);
+    video.hide();
+
+}
+
+function start() {
+    objectDetector = ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects"
+    input_text = document.getElementById("input_id").value;
+}
+function modelLoaded() {
+    console.log("model_loaded!");
+    staus = true
+}
+function draw() {
+    image(video,0,0,300,250);
+}
